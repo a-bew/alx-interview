46cd3687a050696ea00b737bb7c21f2dbc93f017
Adetola Bewaji <adetola013@gmail.com>
2023-11-09 12:36:22 +0100

    introduce promise to make the call asyncronous

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,24 @@
 #!/usr/bin/node
+/* prints all characters of star wars movie passed as commandline argument */
 
 const request = require('request');
 
-function getMovieCharacters(movieId) {
-    // Define the base URL for the Star Wars API
-    const baseUrl = "https://swapi-api.alx-tools.com/api/films/";
-
-    // Make a request to the API to get the movie information
-    request(baseUrl + movieId, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const movieData = JSON.parse(body);
-            const characters = movieData.characters || [];
-
-            if (characters.length > 0) {
-                characters.forEach(characterUrl => {
-                    // Get character information
-                    request(characterUrl, (error, characterResponse, characterBody) => {
-                        if (!error && characterResponse.statusCode === 200) {
-                            const characterData = JSON.parse(characterBody);
-                            const characterName = characterData.name;
-                            console.log(characterName);
-                        } else {
-                            console.log(`Failed to fetch character data for ${characterUrl}`);
-                        }
-                    });
-                });
-            } else {
-                console.log("No characters found for this movie.");
-            }
-        } else {
-            console.log("Failed to fetch movie data.");
-        }
-    });
-}
-
-// Usage: node script.js <Movie ID>
-if (process.argv.length !== 3) {
-    console.log("Usage: node script.js <Movie ID>");
-    process.exit(1);
-}
-
 const movieId = process.argv[2];
-getMovieCharacters(movieId);
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+    err && console.log(err);
+
+    const charactersArray = (JSON.parse(res.body).characters);
+    for (const character of charactersArray) {
+        await new Promise((resolve, reject) => {
+            request(character, (err, res, body) => {
+                err && console.log(err);
+
+                console.log(JSON.parse(body).name);
+                resolve();
+            });
+        });
+    }
+});
