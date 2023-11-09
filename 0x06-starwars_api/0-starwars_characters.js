a3f004ca9e8adc9c2346b007f62e099dca1477ae
Adetola Bewaji <adetola013@gmail.com>
2023-11-09 12:22:19 +0100

    startwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,42 @@
+const request = require('request');
+
+function getMovieCharacters(movieId) {
+    // Define the base URL for the Star Wars API
+    const baseUrl = "https://swapi-api.alx-tools.com/api/films/";
+
+    // Make a request to the API to get the movie information
+    request(baseUrl + movieId, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const movieData = JSON.parse(body);
+            const characters = movieData.characters || [];
+
+            if (characters.length > 0) {
+                characters.forEach(characterUrl => {
+                    // Get character information
+                    request(characterUrl, (error, characterResponse, characterBody) => {
+                        if (!error && characterResponse.statusCode === 200) {
+                            const characterData = JSON.parse(characterBody);
+                            const characterName = characterData.name;
+                            console.log(characterName);
+                        } else {
+                            console.log(`Failed to fetch character data for ${characterUrl}`);
+                        }
+                    });
+                });
+            } else {
+                console.log("No characters found for this movie.");
+            }
+        } else {
+            console.log("Failed to fetch movie data.");
+        }
+    });
+}
+
+// Usage: node script.js <Movie ID>
+if (process.argv.length !== 3) {
+    console.log("Usage: node script.js <Movie ID>");
+    process.exit(1);
+}
+
+const movieId = process.argv[2];
+getMovieCharacters(movieId);
---
+++ 0x06-starwars_api/movie.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+
+
+def get_movie_characters(movie_id):
+    # Define the base URL for the Star Wars API
+    base_url = "https://swapi-api.alx-tools.com/api/films/"
+
+    # Make a request to the API to get the movie information
+    response = requests.get(base_url + str(movie_id))
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        movie_data = response.json()
+        characters = movie_data.get("characters", [])
+
+        if characters:
+            for character_url in characters:
+                # Get character information
+                character_response = requests.get(character_url)
+                if character_response.status_code == 200:
+                    character_data = character_response.json()
+                    character_name = character_data.get("name")
+                    print(character_name)
+                else:
+                    print(
+                        f"Failed to fetch character data for {character_url}")
+        else:
+            print("No characters found for this movie.")
+    else:
+        print("Failed to fetch movie data.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <Movie ID>")
+        sys.exit(1)
+
+    movie_id = sys.argv[1]
+    get_movie_characters(movie_id)
